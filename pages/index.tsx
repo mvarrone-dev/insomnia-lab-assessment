5ccd5692149dfb38c7dc7ae9540f68806eaf7dc5
Michael Varrone <mvarrone.dev@gmail.com>
2023-07-12 02:26:20 -0700

    feat: Toggle visibility of currencies

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,9 @@ export default function Home() {
   const [refreshInterval, setRefreshInterval] = useState<number>(
     DEFAULT_REFRESH_INTERVAL
   );
+  const [hideCurrencies, setHideCurrencies] = useState<
+    Record<string, boolean>
+  >({});
 
   useEffect(() => {
     const timerID = setInterval(() => fetchData(), refreshInterval);
@@ -26,30 +29,55 @@ export default function Home() {
     setIsLoading(false);
   };
 
+  const toggleDisplayCurrency = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const currency = e.target.name;
+    setHideCurrencies({
+      ...hideCurrencies,
+      [currency]: !e.target.checked,
+    });
+  };
+
   return (
     <div className="container mx-auto px-5">
       <h1 className="text-2xl font-bold lg:mt-12 mt-6 mb-8">
         Bitcoin Price Index
       </h1>
-      <div>
-        Refresh the data every{" "}
-        <select
-          value={refreshInterval}
-          onChange={(e) => setRefreshInterval(+e.target.value)}
-          className="border border-gray-300 focus-within:outline-0 rounded px-2 py-1"
-        >
-          <option value={5000}>5</option>
-          <option value={10000}>10</option>
-          <option value={15000}>15</option>
-        </select>{" "}
-        seconds.
+      <div className="flex flex-wrap space-y-3 items-center justify-between lg:mb-5 mb-3">
+        <div>
+          Refresh the data every{" "}
+          <select
+            value={refreshInterval}
+            onChange={(e) => setRefreshInterval(+e.target.value)}
+            className="border border-gray-300 focus-within:outline-0 rounded px-2 py-1"
+          >
+            <option value={5000}>5</option>
+            <option value={10000}>10</option>
+            <option value={15000}>15</option>
+          </select>{" "}
+          seconds.
+        </div>
+        <div className="flex gap-4">
+          {CURRENCIES.map((currency) => (
+            <label key={currency}>
+              <input
+                type="checkbox"
+                name={currency}
+                checked={!hideCurrencies[currency]}
+                onChange={toggleDisplayCurrency}
+                className="mr-2 accent-[#000]"
+              />
+              {currency}
+            </label>
+          ))}
+        </div>
       </div>
       <div className="grid lg:grid-cols-3 grid-cols-1 lg:gap-12 gap-2">
         {CURRENCIES.map(
           (currency) =>
-            !!rateInfo[currency] && (
+            !!rateInfo[currency] &&
+            !hideCurrencies[currency] && (
               <RateCard
-                title="USD"
+                title={currency}
                 total={rateInfo[currency].rate}
                 isLoading={isLoading}
               />
